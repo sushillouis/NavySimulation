656d26931c299eaad60a043d0cbe246fbb4593ea
Liam Francisco <98508195+francisco-liam@users.noreply.github.com>
2024-04-12 11:06:49 -0700

    created new scene with singapore

---
+++ Assets/Singapore/Scripts/DayNightMaterialUpdater.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class DayNightMaterialUpdater : MonoBehaviour
+{
+    public Material dayMaterial;
+    public Material nightMaterial;
+    public GameObject gameObj;
+    
+    public bool isNightTime = false;
+    private bool lastBuildingState = false;
+    private void Update()
+    {
+        if (lastBuildingState != isNightTime)
+        {
+            lastBuildingState = isNightTime;
+
+            if (isNightTime)
+            {
+                gameObj.GetComponent<MeshRenderer>().material = nightMaterial;
+            }
+            else
+            {
+                gameObj.GetComponent<MeshRenderer>().material = dayMaterial;
+            }
+            
+        }
+    }
+}
---
+++ Assets/Singapore/Scripts/MaterialVisibilityUpdater.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+
+public class MaterialVisibilityUpdater : MonoBehaviour {
+
+    public Material defaultMaterial;
+    public Material invisibleMaterial;
+    public GameObject gameObj;
+    
+
+    public bool isVisible = true;
+    private bool lastState;
+
+    private void Update()
+    {
+        if (lastState != isVisible)
+        {
+            lastState = isVisible;
+
+            if (isVisible)
+            {
+                gameObj.GetComponent<MeshRenderer>().material = defaultMaterial;
+            }
+            else
+            {
+                MeshRenderer mr = gameObj.GetComponent<MeshRenderer>();
+                defaultMaterial = mr.material;
+                mr.material = invisibleMaterial;
+            }
+            
+        }
+    }
+}
